66f84274d8b152ce0ca875cbda7d65397df3f145
user <user>
2020-03-29 15:03:24 +0000

    Added a graphql schema, resolvers and the Apollo-Server for about API.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+typeDefs = `
+type Query {
+  about: String!
+}
+type Mutation {
+  setAboutMessage(message: String!): String
+}
+`;
+
+const resolvers = {
+    Query: {
+	about: () => aboutMessage,
+    },
+    Mutation: {
+	setAboutMessage,
+    }
+};
+
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+}
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World');
-});
+apolloServer.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(3000, function () {
   console.log('App started on port 3000');
