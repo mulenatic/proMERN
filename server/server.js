7a1283177656fd4b298068db971d2b421bb7b578
user <user>
2020-03-29 15:53:14 +0000

    Moved the schema into a separate file.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const fs = require('fs');
 
 let aboutMessage = "Issue Tracker API v1.0";
-
-typeDefs = `
-type Query {
-  about: String!
-}
-type Mutation {
-  setAboutMessage(message: String!): String
-}
-`;
 
 const resolvers = {
     Query: {
@@ -26,7 +18,7 @@ function setAboutMessage(_, { message }) {
 }
 
 const apolloServer = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
     resolvers,
 });
 
