d81d1723737e5831f683fdf0e0dbf464903705a2
user <user>
2020-04-01 11:34:35 +0000

    Using a collection and adding a single record.

--- scripts/trymongo.js
+++ scripts/trymongo.js
@@ -5,4 +5,11 @@ const url = 'mongodb://mongo/issuetracker';
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(function(err, client) {
     const db = client.db();
+
+    const collection = db.collection('employees');
+
+    const employee = { id: 5, name: 'A. Callback', age: 23 };
+    collection.insertOne(employee, function(err, result) {
+	console.log('Result of insert:\n', result.insertedId);
+    });
 });
