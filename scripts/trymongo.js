c6298479f422ca4d3c01f61fe00b25d6d609ba32
user <user>
2020-04-01 11:45:06 +0000

    Wrapped the calls in a function and added error handling.

--- scripts/trymongo.js
+++ scripts/trymongo.js
@@ -2,27 +2,51 @@ const { MongoClient } = require('mongodb');
 
 const url = 'mongodb://mongo/issuetracker';
 
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(function(err, client) {
-    const db = client.db();
+function testWithCallbacks(callback) {
 
-    const collection = db.collection('employees');
+    console.log('\n--- testWithCallbacks ---\n');
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    const employee = { id: 8, name: 'A. Callback', age: 23 };
-    collection.insertOne(employee, function(err, result) {
-	console.log('Result of insert:\n', result.insertedId);
+    client.connect(function(err, client) {
+	if (err) {
+	    callback(err);
+	    return;
+	}
+	console.log('Connected to MongoDB');
+	
+	const db = client.db();
 
+	const collection = db.collection('employees');
 
-    collection.find({ _id: result.insertedId })
-	      .toArray(function(err, docs) {
-		  console.log('Result of find:\n', docs);
+	const employee = { id: 9, name: 'A. Callback', age: 23 };
+	collection.insertOne(employee, function(err, result) {
+	    if (err) {
+		client.close();
+		callback(err);
+		return;
+	    }
+	    console.log('Result of insert:\n', result.insertedId);
 
-		  client.close();
-	      });
+	    collection.find({ _id: result.insertedId })
+		      .toArray(function(err, docs) {
+			  if (err) {
+			      client.close();
+			      callback(err);
+			      return;
+			  }
+			  console.log('Result of find:\n', docs);
+			  client.close();
+			  callback(err);
+		      });
 
+	});
+	
     });
-    
+
+}
+
+testWithCallbacks(function(err) {
+    if(err) {
+	console.log(err);
+    }
 });
-
-
-
