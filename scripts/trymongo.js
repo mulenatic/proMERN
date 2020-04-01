1bf922816e16b7327b08a8cab62a9b1894e7d926
user <user>
2020-04-01 12:03:17 +0000

    Using the async/await paradigm to simplify the code.

--- scripts/trymongo.js
+++ scripts/trymongo.js
@@ -2,51 +2,32 @@ const { MongoClient } = require('mongodb');
 
 const url = 'mongodb://mongo/issuetracker';
 
-function testWithCallbacks(callback) {
+async function testWithAsync() {
 
-    console.log('\n--- testWithCallbacks ---\n');
+    console.log('\n--- testWithAsync ---\n');
     const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    client.connect(function(err, client) {
-	if (err) {
-	    callback(err);
-	    return;
-	}
+    try {
+
+	await client.connect();
 	console.log('Connected to MongoDB');
 	
 	const db = client.db();
-
 	const collection = db.collection('employees');
 
 	const employee = { id: 9, name: 'A. Callback', age: 23 };
-	collection.insertOne(employee, function(err, result) {
-	    if (err) {
-		client.close();
-		callback(err);
-		return;
-	    }
-	    console.log('Result of insert:\n', result.insertedId);
+	const result = await collection.insertOne(employee);
+	console.log('Result of insert:\n', result.insertedId);
 
-	    collection.find({ _id: result.insertedId })
-		      .toArray(function(err, docs) {
-			  if (err) {
-			      client.close();
-			      callback(err);
-			      return;
-			  }
-			  console.log('Result of find:\n', docs);
-			  client.close();
-			  callback(err);
-		      });
+	const docs = await collection.find({ _id: result.insertedId }).toArray();
+	console.log('Result of find:\n', docs);
 
-	});
-	
-    });
+    } catch(err) {
+	console.log(err);
+    } finally {
+	client.close();
+    }
 
 }
 
-testWithCallbacks(function(err) {
-    if(err) {
-	console.log(err);
-    }
-});
+testWithAsync();
