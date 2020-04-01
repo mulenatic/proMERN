6669370db0bfac9c776b4d4183268d27baebcc1c
user <user>
2020-04-01 11:24:48 +0000

    Using new Url Parser to omit error messages.
    Note: the current server discover and monitoring engine is
    deprecated. Passing useUnifiedTopology: true in the MongoClient
    constructor removes the info message.

--- scripts/trymongo.js
+++ scripts/trymongo.js
@@ -2,5 +2,5 @@ const { MongoClient } = require('mongodb');
 
 const url = 'mongodb://mongo/issuetracker';
 
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useNewUrlParser: true });
 client.connect();
