365699250c9163a6d075e5c9af57f19787aafbc6
mulenatic <mulenatic@gmail.com>
2020-04-03 12:51:06 +0000

    Added a http-proxy in the ui-server. Not working.

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,14 +1,23 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const proxy = require('http-proxy-middleware');
 
-const port = process.env.UI_SERVER_PORT || 4000;
+
+    const port = process.env.UI_SERVER_PORT || 4000;
 
 const app = express();
 
 app.use(express.static('public'));
 
+const apiProxyTarget = process.env.API_PROXY_TARGET;
+if (apiProxyTarget) {
+    console.log(`use proxy on ${apiProxyTarget}`);
+    app.use('/graphql', proxy({ target: apiProxyTarget }));
+}
+
 const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
 const env = { UI_API_ENDPOINT };
+console.log(`UI_API_ENDPOINT = ${UI_API_ENDPOINT}`);
 
 app.get('/env.js', (req, res) => res.send(`window.ENV = ${JSON.stringify(env)}`));
 
