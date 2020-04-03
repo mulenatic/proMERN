15c732216dae69f9bbef53ba424bafd7f87fcee8
user <user>
2020-04-03 12:01:08 +0000

    Moved URL and Ports in the ui to env files.
    For the server, use the same approach as for the api server.
    
    For the backend, a "Javascript file" is generated withing uiserver.js,
    with data, loaded from the env file. The JS file publishes the values
    in the window.ENV global variable.

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(express.static('public'));
 
+const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
+const env = { UI_API_ENDPOINT };
+
+app.get('/env.js', (req, res) => res.send(`window.ENV = ${JSON.stringify(env)}`));
+
 app.listen(port, function() {
     console.log(`UI stated on port ${port}`);
 });
