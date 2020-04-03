6f46f694ed309e8171a14a2b26ac4675c018c4cf
user <user>
2020-04-03 07:08:14 +0000

    Introduced the dotenv package and removed parameters from api-server files.

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,9 +1,12 @@
 const express = require('express');
+require('dotenv').config();
+
+const port = process.env.UI_SERVER_PORT || 4000;
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(4000, function() {
-    console.log('UI stated on port 4000');
+app.listen(${port}, function() {
+    console.log(`UI stated on port ${port}`);
 });
