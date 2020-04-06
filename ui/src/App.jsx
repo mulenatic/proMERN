cb38c4f133895a8f4c15d36c34f33e32e8ebbae3
mulenatic <user>
2020-04-06 14:22:50 +0000

    Moved graphQlfetch to separate file and used webpack to bundle everything.

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -3,6 +3,8 @@
 /* eslint "react/jsx-no-undef": "off" */
 /* eslint "react/no-multi-comp": "off" */
 
+import graphQLFetch from './graphQLFetch';
+
 // eslint-disable-next-line react/prefer-stateless-function
 class IssueFilter extends React.Component {
   render() {
@@ -12,38 +14,6 @@ class IssueFilter extends React.Component {
   }
 }
 
-const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
-
-function jsonDataReviver(key, value) {
-  if (dateRegex.test(value)) return new Date(value);
-  return value;
-}
-
-async function graphQLFetch(query, variables = {}) {
-  try {
-    const response = await fetch(window.ENV.UI_API_ENDPOINT, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ query, variables }),
-    });
-    const body = await response.text();
-    const result = JSON.parse(body, jsonDataReviver);
-
-    if (result.errors) {
-      const error = result.errors[0];
-      if (error.extensions.code === 'BAD_USER_INPUT') {
-        const details = error.extensions.exception.errors.join('\n ');
-        alert(`${error.message}: \n ${details}`);
-      } else {
-        alert(`${error.extensions.code}: ${error.message}`);
-      }
-    }
-    return result.data;
-  } catch (e) {
-    alert(`Error in sending data to server: ${e.message}`);
-    return undefined;
-  }
-}
 
 function IssueRow({ issue }) {
   return (
@@ -131,7 +101,6 @@ class IssueList extends React.Component {
 }`;
 
     const data = await graphQLFetch(query);
-    console.log(`loadData: ${JSON.stringify(data)}`);
     if (data) {
       this.setState({ issues: data.issueList });
     }
