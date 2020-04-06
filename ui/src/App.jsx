ceda9695605a63e507b9452b323cd459de925edf
mulenatic <user>
2020-04-06 07:26:52 +0000

    Removing eslint errors from App.jsx.
    Since React for now is imported via CDN, 'React' must be in scope
    errors are disabled using comments.
    IssueFilter is a placeholder and therefore not converted to stateless
    function.
    Prefer destructuring, especially props assignment:
    For now, more than one component is declared in the file. Disable
    error.
    Props validation errors are disabled for all files.
    
    Solved the other errors.

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,169 +1,172 @@
+/* eslint "react/react-in-jsx-scope": "off" */
+/* globals React ReactDOM */
+/* eslint "react/jsx-no-undef": "off" */
+/* eslint "react/no-multi-comp": "off" */
+
+// eslint-disable-next-line react/prefer-stateless-function
 class IssueFilter extends React.Component {
-    render() {
-	return (
-	    <div>This is a placeholder for the issue filter.</div>
-	);
-    }
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+    );
+  }
 }
 
 const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
 
-function jsonDataReviver (key, value) {
-    if (dateRegex.test(value)) return new Date(value);
-    return value;
+function jsonDataReviver(key, value) {
+  if (dateRegex.test(value)) return new Date(value);
+  return value;
 }
 
 async function graphQLFetch(query, variables = {}) {
-    try{
-	const response = await fetch(window.ENV.UI_API_ENDPOINT, {
-	    method: 'POST',
-	    headers: { 'Content-Type': 'application/json' },
-	    body: JSON.stringify({ query, variables })
-	});
-	const body = await response.text();
-	const result = JSON.parse(body, jsonDataReviver);
+  try {
+    const response = await fetch(window.ENV.UI_API_ENDPOINT, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query, variables }),
+    });
+    const body = await response.text();
+    const result = JSON.parse(body, jsonDataReviver);
 
-	if (result.errors) {
-	    const error = result.errors[0];
-	    if (error.extensions.code == 'BAD_USER_INPUT') {
-		const details = error.extensions.exception.errors.join('\n ');
-		alert(`${error.message}: \n ${details}`);
-	    } else {
-		alert(`${error.extensions.code}: ${error.message}`);
-	    }
-	}
-	return result.data;
-	
-    } catch(e) {
-	alert(`Error in sending data to server: ${e.message}`);
+    if (result.errors) {
+      const error = result.errors[0];
+      if (error.extensions.code === 'BAD_USER_INPUT') {
+        const details = error.extensions.exception.errors.join('\n ');
+        alert(`${error.message}: \n ${details}`);
+      } else {
+        alert(`${error.extensions.code}: ${error.message}`);
+      }
     }
+    return result.data;
+  } catch (e) {
+    alert(`Error in sending data to server: ${e.message}`);
+    return undefined;
+  }
 }
 
-
-function IssueRow(props) {
-    const issue = props.issue;
-    return (
-	<tr>
-	    <td>{issue.id}</td>
-	    <td>{issue.status}</td>
-	    <td>{issue.owner}</td>
-	    <td>{issue.created.toDateString()}</td>
-	    <td>{issue.effort}</td>
-	    <td>{issue.due ? issue.due.toDateString() : ' '}</td>
-	    <td>{issue.title}</td>
-	</tr>
-    );
+function IssueRow({ issue }) {
+  return (
+    <tr>
+      <td>{issue.id}</td>
+      <td>{issue.status}</td>
+      <td>{issue.owner}</td>
+      <td>{issue.created.toDateString()}</td>
+      <td>{issue.effort}</td>
+      <td>{issue.due ? issue.due.toDateString() : ' '}</td>
+      <td>{issue.title}</td>
+    </tr>
+  );
 }
 
-function IssueTable(props) {
-    const issueRows = props.issues.map(issue =>
-	<IssueRow key={issue.id} issue={issue} />
-    );
-
-    return (
-	<table className="bordered-table">
-	    <thead>
-		<tr>
-		    <th>ID</th>
-		    <th>Status</th>
-		    <th>Owner</th>
-		    <th>Created</th>
-		    <th>Effort</th>
-		    <th>Due Date</th>
-		    <th>Title</th>
-		</tr>
-	    </thead>
-	    <tbody>
-		{issueRows}
-	    </tbody>
-	</table>
-    );
+function IssueTable({ issues }) {
+  const issueRows = issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+  return (
+    <table className="bordered-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Status</th>
+          <th>Owner</th>
+          <th>Created</th>
+          <th>Effort</th>
+          <th>Due Date</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        {issueRows}
+      </tbody>
+    </table>
+  );
 }
 
 class IssueAdd extends React.Component {
-    constructor() {
-	super();
-	this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleSubmit(e) {
-	e.preventDefault();
-	const form = document.forms.issueAdd;
-	const issue = {
-	    owner: form.owner.value, title: form.title.value,
-	    due: new Date(new Date().getTime() + 1000*60*60*24*10),
-	}
-	this.props.createIssue(issue);
-	form.owner.value = ""; form.title.value = "";
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
+    const issue = {
+      owner: form.owner.value,
+      title: form.title.value,
+      due: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 10),
+    };
+    this.props.createIssue(issue);
+    form.owner.value = '';
+    form.title.value = '';
+  }
 
-    render() {
-	return (
-	    <form name="issueAdd" onSubmit={this.handleSubmit}>
-		<input type="text" name="owner" placeholder="Owner" />
-		<input type="text" name="title" placeholder="Title" />
-		<button>Add</button>
-	    </form>
-	);
-    }
+  render() {
+    return (
+      <form name="issueAdd" onSubmit={this.handleSubmit}>
+        <input type="text" name="owner" placeholder="Owner" />
+        <input type="text" name="title" placeholder="Title" />
+        <button type="submit">Add</button>
+      </form>
+    );
+  }
 }
 
 class IssueList extends React.Component {
-    constructor() {
-	super();
-	this.state = { issues: [] };
-	this.createIssue = this.createIssue.bind(this);
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  async loadData() {
+    const query = `query {
+    issueList {
+        id title status owner
+        created effort due
     }
+}`;
 
-    componentDidMount() {
-	this.loadData();
+    const data = await graphQLFetch(query);
+    console.log(`loadData: ${JSON.stringify(data)}`);
+    if (data) {
+      this.setState({ issues: data.issueList });
     }
+  }
 
-    async loadData() {
-	const query = `query {
-	    issueList {
-		id title status owner
-		created effort due
-	    }
-	}`;
-
-	const data = await graphQLFetch(query);
-	console.log(`loadData: ${JSON.stringify(data)}`);
-	if (data) {
-	    this.setState({ issues: data.issueList });
-	}
+  async createIssue(issue) {
+    const query = `mutation {
+    issueAdd(issue: {
+title: "${issue.title}",
+owner: "${issue.owner}",
+due: "${issue.due.toISOString()}",
+    }) {
+id
     }
+}`;
 
-    async createIssue(issue) {
-	const query = `mutation {
-	    issueAdd(issue: {
-		title: "${issue.title}",
-		owner: "${issue.owner}",
-		due: "${issue.due.toISOString()}",
-	    }) {
-		id
-	    }
-	}`;
-
-	const data = await graphQLFetch(query, { issue });
-	if(data) {
-	    this.loadData();
-	}
-
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
     }
+  }
 
-    render() {
-	return (
-	    <React.Fragment>
-		<h1>Issue Tracker</h1>
-		<IssueFilter />
-		<hr />
-		<IssueTable issues={this.state.issues} />
-		<hr />
-		<IssueAdd createIssue={this.createIssue} />
-	    </React.Fragment>
-	);
-    }
+  render() {
+    const { issues } = this.state;
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={issues} />
+        <hr />
+        <IssueAdd createIssue={this.createIssue} />
+      </React.Fragment>
+    );
+  }
 }
 
 const element = <IssueList />;
