bda45b25704708cf7a8b0c335d6e241b7e48e5f3
mulenatic <user>
2020-04-06 12:57:02 +0000

    Moved the issue realted functions into a separate module.

--- api/server.js
+++ api/server.js
@@ -1,42 +1,8 @@
 const express = require('express');
-const { UserInputError } = require('apollo-server-express');
-const fs = require('fs');
 
 require('dotenv').config();
 
 const port = process.env.API_SERVER_PORT || 3000;
-
-
-async function issueList() {
-  const issues = await db.collection('issues').find({}).toArray();
-  return issues;
-}
-
-function validateIssue(issue) {
-  const errors = [];
-  if (issue.title.length < 3) {
-    errors.push('Field "title" must be at least 3 characters long.');
-  }
-  if (issue.status === 'Assigned' && !issue.owner) {
-    errors.push('Field "owner" is required when status is "Assigned"');
-  }
-  if (errors.length > 0) {
-    throw new UserInputError('Invalid input(s)', { errors });
-  }
-}
-
-async function issueAdd(_, { issue }) {
-  validateIssue(issue);
-
-  const newIssue = Object.assign({}, issue);
-  newIssue.created = new Date();
-  newIssue.id = await getNextSequence('issues');
-  if (issue.status === undefined) newIssue.status = 'New';
-
-  const result = await db.collection('issues').insertOne(newIssue);
-  const savedIssue = await db.collection('issues').findOne({ _id: result.insertedId });
-  return savedIssue;
-}
 
 const app = express();
 
