d1d58ac63e9688286014ec16cc2266c24a41961b
mulenatic <user>
2020-04-07 08:58:33 +0000

    Enabled hot deployment in development.
    The change in App.jsx is needed, so that changes in a component will
    be accepted by the component tree. Otherwise, the page is not reloaded.

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -5,6 +5,26 @@ const port = process.env.UI_SERVER_PORT || 4000;
 
 const app = express();
 
+const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
+
+if (enableHMR && (process.env.NODE_ENV !== 'production')) {
+  console.log('Adding dev middleware, enabling HMR');
+  /* eslint "global-require": "off" */
+  /* eslint "import/no-extraneous-dependencies": "off" */
+  const webpack = require('webpack');
+  const devMiddleware = require('webpack-dev-middleware');
+  const hotMiddleware = require('webpack-hot-middleware');
+
+  const config = require('./webpack.config.js');
+  config.entry.app.push('webpack-hot-middleware/client');
+  config.plugins = config.plugins || [];
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  const compiler = webpack(config);
+  app.use(devMiddleware(compiler));
+  app.use(hotMiddleware(compiler));
+}
+
 app.use(express.static('public'));
 
 const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
