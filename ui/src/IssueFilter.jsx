5653dcb7c3fe48a6325343488fe1e407c13f911b
mulenatic <user>
2020-04-07 06:47:38 +0000

    Moved every component into a separate file.
    Interestingly, the jsx file ending has to be added in the import so
    that webpack can find the files.

---
+++ ui/src/IssueFilter.jsx
@@ -0,0 +1,10 @@
+/* globals React */
+/* eslint-disable-next-line react/prefer-stateless-function */
+
+export default class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+    );
+  }
+}
---
+++ ui/src/IssueList.jsx
@@ -0,0 +1,64 @@
+/* globals React */
+/* eslint "react/jsx-no-undef": "off" */
+
+import IssueFilter from './IssueFilter.jsx';
+import IssueTable from './IssueTable.jsx';
+import IssueAdd from './IssueAdd.jsx';
+import graphQLFetch from './graphQLFetch';
+
+export default class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  async loadData() {
+    const query = `query {
+    issueList {
+        id title status owner
+        created effort due
+    }
+}`;
+
+    const data = await graphQLFetch(query);
+    if (data) {
+      this.setState({ issues: data.issueList });
+    }
+  }
+
+  async createIssue(issue) {
+    const query = `mutation {
+    issueAdd(issue: {
+title: "${issue.title}",
+owner: "${issue.owner}",
+due: "${issue.due.toISOString()}",
+    }) {
+id
+    }
+}`;
+
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
+    }
+  }
+
+  render() {
+    const { issues } = this.state;
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={issues} />
+        <hr />
+        <IssueAdd createIssue={this.createIssue} />
+      </React.Fragment>
+    );
+  }
+}
