279f13db35d565e7b3d227a3ce3b3109464cf39a
mulenatic <user>
2020-04-08 13:25:37 +0000

    Added IssueDetail component with a nested route.

--- ui/src/IssueList.jsx
+++ ui/src/IssueList.jsx
@@ -2,9 +2,12 @@
 
 import React from 'react';
 import URLSearchParams from 'url-search-params';
+import { Route } from 'react-router-dom';
+
 import IssueFilter from './IssueFilter.jsx';
 import IssueTable from './IssueTable.jsx';
 import IssueAdd from './IssueAdd.jsx';
+import IssueDetail from './IssueDetail.jsx';
 import graphQLFetch from './graphQLFetch';
 
 
@@ -65,6 +68,7 @@ id
 
   render() {
     const { issues } = this.state;
+    const { match } = this.props;
     return (
       <React.Fragment>
         <h1>Issue Tracker</h1>
@@ -73,6 +77,8 @@ id
         <IssueTable issues={issues} />
         <hr />
         <IssueAdd createIssue={this.createIssue} />
+        <hr />
+        <Route path={`${match.path}/:id`} component={IssueDetail} />
       </React.Fragment>
     );
   }
