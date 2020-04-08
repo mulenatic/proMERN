ef2784b6f2b0db52b1e19a107062ea9dcdc1f544
mulenatic <user>
2020-04-08 09:41:43 +0000

    Replaced the anchors with Links and NavLinks.

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,15 +1,16 @@
 /* eslint "react/prefer-stateless-function": "off" */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class IssueFilter extends React.Component {
   render() {
     return (
       <div>
-        <a href="/#/issues">All issues</a>
+        <Link to="/issues">All issues</Link>
         {' | '}
-        <a href="/#/issues?status=New">New Issues</a>
+        <Link to="/issues?status=New">New Issues</Link>
         {' | '}
-        <a href="/#/issues?status=Assigned">Assigned Issues</a>
+        <Link to="/issues?status=Assigned">Assigned Issues</Link>
       </div>
     );
   }
