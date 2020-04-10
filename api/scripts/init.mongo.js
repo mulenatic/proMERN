4e03c627969b854194abcebcac8108cebbf5c829
mulenatic <user>
2020-04-10 10:01:24 +0000

    Using the delete API in the UI.

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -2,6 +2,7 @@
 /* eslint no-restricted-globals: "off" */
 
 db.issues.remove({});
+db.deleted_issues.remove({});
 
 const issuesDB = [
   {
@@ -45,3 +46,5 @@ db.issues.createIndex({ id: 1 }, { unique: true });
 db.issues.createIndex({ status: 1 });
 db.issues.createIndex({ owner: 1 });
 db.issues.createIndex({ created: 1 });
+
+db.deleted_issues.createIndex({ id: 1 }, { unique: true });
