0e375de545c4b7cf03e8b1f99d36d2b8c75f8dee
mulenatic <user>
2020-04-12 09:02:56 +0000

    Included a Navbar and Footer.

--- ui/src/IssueList.jsx
+++ ui/src/IssueList.jsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import URLSearchParams from 'url-search-params';
 import { Route } from 'react-router-dom';
-import { Label } from 'react-bootstrap';
 
 import IssueFilter from './IssueFilter.jsx';
 import IssueTable from './IssueTable.jsx';
@@ -129,7 +128,6 @@ effort created due description
     const { match } = this.props;
     return (
       <React.Fragment>
-        <h1><Label>Issue Tracker</Label></h1>
         <IssueFilter />
         <hr />
         <IssueTable issues={issues} closeIssue={this.closeIssue} deleteIssue={this.deleteIssue} />
