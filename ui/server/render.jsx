0c28405826e51a5029cd87751447c2a3ecb13d25
mulenatic <user>
2020-04-16 14:17:40 +0000

    Modify renderer to fetch data from static method fetchData of component.

--- ui/server/render.jsx
+++ ui/server/render.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
+import { StaticRouter, matchPath } from 'react-router-dom';
 
 import Page from '../src/Page.jsx';
 import template from './template.js';
 import store from '../src/store.js';
-import About from '../src/About.jsx';
+import routes from '../src/routes.js';
 
 async function render(req, res) {
-  const resultData = About.fetchData();
-  store.initialData = resultData;
+  const activeRoute = routes.find(route => matchPath(req.path, route));
+
+  let initialData;
+  if (activeRoute && activeRoute.component.fetchData) {
+    initialData = await activeRoute.component.fetchData();
+  }
+  store.initialData = initialData;
   const element = (
     <StaticRouter location={req.url} context={{}}>
       <Page />
     </StaticRouter>
   );
   const body = ReactDOMServer.renderToString(element);
-  res.send(template(body, resultData));
+  res.send(template(body, initialData));
 }
 
 export default render;
