c72724f933b8c74463dc5f54a4fa85457ac1a642
mulenatic <user>
2020-04-26 10:02:11 +0000

    Using context in stateless component. This is more elaborate.

--- ui/src/IssueTable.jsx
+++ ui/src/IssueTable.jsx
@@ -1,74 +1,84 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Button, Glyphicon, Tooltip, OverlayTrigger, Table, } from 'react-bootstrap';
+import {
+  Button, Glyphicon, Tooltip, OverlayTrigger, Table,
+} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import UserContext from './UserContext';
 
-const IssueRow = withRouter(({
-  issue,
-  location: { search },
-  closeIssue,
-  deleteIssue,
-  index,
-}) => {
-  const selectLocation = { pathname: `/issues/${issue.id}`, search };
-  const editTooltip = (
-    <Tooltip id="edit-tooltip" placement="top">Edit Issue</Tooltip>
-  );
-  const closeTooltip = (
-    <Tooltip id="close-tooltip" placement="top">Close Issue</Tooltip>
-  );
-  const deleteTooltip = (
-    <Tooltip id="delete-tooltip" placement="top">Delete Issue</Tooltip>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class IssueRowPlain extends React.Component {
+  render() {
+    const {
+      issue, location: { search }, closeIssue, deleteIssue, index,
+    } = this.props;
+    const user = this.context;
+    const disabled = !user.signedIn;
 
-  function onClose(e) {
-    e.preventDefault();
-    closeIssue(index);
-  }
+    const selectLocation = { pathname: `/issues/${issue.id}`, search };
+    const editTooltip = (
+      <Tooltip id="edit-tooltip" placement="top">Edit Issue</Tooltip>
+    );
+    const closeTooltip = (
+      <Tooltip id="close-tooltip" placement="top">Close Issue</Tooltip>
+    );
+    const deleteTooltip = (
+      <Tooltip id="delete-tooltip" placement="top">Delete Issue</Tooltip>
+    );
 
-  function onDelete(e) {
-    e.preventDefault();
-    deleteIssue(index);
-  }
+    function onClose(e) {
+      e.preventDefault();
+      closeIssue(index);
+    }
 
-  const tableRow = (
-    <tr>
-      <td>{issue.id}</td>
-      <td>{issue.status}</td>
-      <td>{issue.owner}</td>
-      <td>{issue.created.toDateString()}</td>
-      <td>{issue.effort}</td>
-      <td>{issue.due ? issue.due.toDateString() : ' '}</td>
-      <td>{issue.title}</td>
-      <td>
-        <LinkContainer to={`/edit/${issue.id}`}>
-          <OverlayTrigger delayShow={1000} overlay={editTooltip}>
-            <Button bsSize="xsmall">
-              <Glyphicon glyph="edit" />
+    function onDelete(e) {
+      e.preventDefault();
+      deleteIssue(index);
+    }
+
+    const tableRow = (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.due ? issue.due.toDateString() : ' '}</td>
+        <td>{issue.title}</td>
+        <td>
+          <LinkContainer to={`/edit/${issue.id}`}>
+            <OverlayTrigger delayShow={1000} overlay={editTooltip}>
+              <Button bsSize="xsmall">
+                <Glyphicon glyph="edit" />
+              </Button>
+            </OverlayTrigger>
+          </LinkContainer>
+          <OverlayTrigger delayShow={1000} overlay={closeTooltip}>
+            <Button disabled={disabled} bsSize="xsmall" onClick={onClose}>
+              <Glyphicon glyph="remove" />
             </Button>
           </OverlayTrigger>
-        </LinkContainer>
-        <OverlayTrigger delayShow={1000} overlay={closeTooltip}>
-          <Button bsSize="xsmall" onClick={onClose}>
-            <Glyphicon glyph="remove" />
-          </Button>
-        </OverlayTrigger>
-        {' '}
-        <OverlayTrigger delayShow={1000} overlay={deleteTooltip}>
-          <Button bsSize="xsmall" onClick={onDelete}>
-            <Glyphicon glyph="trash" />
-          </Button>
-        </OverlayTrigger>
-      </td>
-    </tr>
-  );
+          {' '}
+          <OverlayTrigger delayShow={1000} overlay={deleteTooltip}>
+            <Button disabled={disabled} bsSize="xsmall" onClick={onDelete}>
+              <Glyphicon glyph="trash" />
+            </Button>
+          </OverlayTrigger>
+        </td>
+      </tr>
+    );
 
-  return (
-    <LinkContainer to={selectLocation}>
-      {tableRow}
-    </LinkContainer>
-  );
-});
+    return (
+      <LinkContainer to={selectLocation}>
+        {tableRow}
+      </LinkContainer>
+    );
+  }
+}
+
+IssueRowPlain.contextType = UserContext;
+const IssueRow = withRouter(IssueRowPlain);
+delete IssueRow.contextType;
 
 export default function IssueTable({ issues, closeIssue, deleteIssue }) {
   const issueRows = issues.map((issue, index) => (
